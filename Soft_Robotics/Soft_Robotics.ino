a5d291fe399ead95d6f4ca2250d3413362e6f8cb
Bram Schork <Bram@Bram-MacBook-Air.local>
2017-05-05 12:57:49 -0400

    first test

--- Soft_Robotics/Soft_Robotics.ino
+++ Soft_Robotics/Soft_Robotics.ino
@@ -1,9 +1,19 @@
+int vavle1 = 10; //left hand
+int vavle2 = 11; //right hand
+//int vavle3 = 12;
+//int vavle4 = 13;
+
 void setup() {
-  // put your setup code here, to run once:
-  
+  pinMode(vavle1, OUTPUT);
+  pinMode(vavle2, OUTPUT);
+  //pinMode(vavle3, OUTPUT);
+  //pinMode(vavle4, OUTPUT);
+  digitalWrite(vavle10, HIGH);
+  digitalWrite(vavle11, HIGH);
 }
-test 2
+
 void loop() {
   // put your main code here, to run repeatedly:
+  
 
 }
