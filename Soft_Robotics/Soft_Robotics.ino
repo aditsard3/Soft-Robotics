dac075ffe5ef5783a627c47c92b6d1476351edfe
Bram Schork <Bram@Bram-Air.fios-router.home>
2017-05-11 22:24:49 -0400

    first air pressure test

--- Soft_Robotics/Soft_Robotics.ino
+++ Soft_Robotics/Soft_Robotics.ino
@@ -1,19 +1,25 @@
-int vavle1 = 10; //left hand
-int vavle2 = 11; //right hand
-//int vavle3 = 12;
-//int vavle4 = 13;
+const int analogInPin = A0;  // Analog input pin that the potentiometer is attached to
 
-void setup() {
-  pinMode(vavle1, OUTPUT);
-  pinMode(vavle2, OUTPUT);
-  //pinMode(vavle3, OUTPUT);
-  //pinMode(vavle4, OUTPUT);
-  digitalWrite(vavle10, HIGH);
-  digitalWrite(vavle11, HIGH);
+int sensorValue = 0;        // value read from the pressure sensor via the amplifier stage
+float outputValue = 0;        // value output to the Serial port and LCD display
+
+void setup() 
+{
+  // initialize serial communications at 9600 bps:
+  Serial.begin(9600); 
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void loop() 
+{
+  // read the analog in value:
+  sensorValue = analogRead(analogInPin);            
+  outputValue = map(sensorValue, 10, 1023, 0, 100); //mapping the sensor - the zero value of sensor is around 10
   
+  Serial.print("sensor = " );                       
+  Serial.print(sensorValue);      
+  Serial.print("\toutput = ");      
+  Serial.println(outputValue);   
 
+  // wait 500 milliseconds before the next loop for the analog-to-digital converter to settle
+  delay(100);                     
 }
