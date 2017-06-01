45b30241fde132aab6063ba75dda691ff5b0176f
Bram Schork <Bram@Bram-MacBook-Air.local>
2017-06-01 11:27:41 -0400

    MOSFET commit

---
+++ MOSFET/MOSFET.ino
@@ -0,0 +1,33 @@
+int mosfetone = 48;
+int mosfettwo = 50;
+int mosfetthree = 52;
+int mosfetfour = 46;
+
+void setup() {
+  // put your setup code here, to run once:
+pinMode(mosfetone, OUTPUT);
+pinMode(mosfettwo, OUTPUT);
+pinMode(mosfetthree, OUTPUT);
+pinMode(mosfetfour, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  
+digitalWrite(mosfetone, HIGH);
+delay(2000);
+digitalWrite(mosfetone, LOW);
+
+digitalWrite(mosfettwo, HIGH);
+delay(2000);
+digitalWrite(mosfettwo, LOW);
+
+digitalWrite(mosfetthree, HIGH);
+delay(2000);
+digitalWrite(mosfetthree, LOW);
+
+digitalWrite(mosfetfour, HIGH);
+delay(2000);
+digitalWrite(mosfetfour, LOW);
+
+}
---
+++ Sensor_Test/Sensor_Test.ino
@@ -0,0 +1,25 @@
+int sensorPin = A0;
+int sensorValue = 0;        // value read from the pressure sensor via the amplifier stage
+int firstFingerValve = 14;
+void setup() 
+{
+  pinMode(14, OUTPUT);
+  Serial.begin(9600); 
+  
+}
+
+void loop() 
+{ 
+  sensorValue = analogRead(sensorPin);            
+  Serial.print("Sensor Value = ");
+  Serial.println(sensorValue);
+
+  if(sensorValue > 23) {
+    digitalWrite(firstFingerValve, LOW);
+  }
+
+  else {
+    digitalWrite(firstFingerValve, HIGH);
+  }
+  delay(750);                     
+}
